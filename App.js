59a9ae7cf73cae8d15bb37e520172c2bf02fcd57
Fabian Schmauder <fabian.schmauder@gmail.com>
2020-11-26 11:12:19 +0100

    Add scroll view

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text, View, ScrollView } from 'react-native';
 import {loadAlbums} from './src/service/album-service'
 
 
@@ -12,20 +12,13 @@ export default function App() {
   },[])
 
   return (
-    <View style={styles.container}>
+    <ScrollView>
       <StatusBar style="auto" hidden />
       {albums.map(album => <View key={album.id}>
         <Text>{album.title}</Text>
       </View>)}
-    </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
