3241a01ada46a10d49e936617086a1c63dc9e85c
Fabian Schmauder <fabian.schmauder@gmail.com>
2020-11-26 14:25:24 +0100

    Add Image picker

--- src/screens/AlbumScreen.js
+++ src/screens/AlbumScreen.js
@@ -1,8 +1,39 @@
-import React from "react";
-import {View, Text} from "react-native";
+import React, {useEffect, useState} from "react";
+import {View, Text, Button,Image} from "react-native";
+import * as ImagePicker from 'expo-image-picker';
 
 export function AlbumScreen({route}) {
+    const [image, setImage] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            if (Platform.OS !== 'web') {
+                const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+                if (status !== 'granted') {
+                    alert('Sorry, we need camera roll permissions to make this work!');
+                }
+            }
+        })();
+    }, []);
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setImage(result.uri);
+        }
+    };
+
     return <View>
         <Text> Album {route.params.id}</Text>
+        <Button title="Pick an image from camera roll" onPress={pickImage} />
+        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
     </View>
 }
